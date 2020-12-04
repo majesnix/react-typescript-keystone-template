bf34d1cb3a5134132176bcb5fa43445b4a9045bf
majesnix <dominic.classen@coduct.io>
2020-12-04 10:57:38 +0100

    chore: fix type

--- src/dataLayer/models/Example.ts
+++ src/dataLayer/models/Example.ts
@@ -2,5 +2,5 @@ import { model, Model, prop } from "mobx-keystone";
 
 @model("Example")
 export default class Example extends Model({
-  id: prop<Maybe<String>>(() => undefined),
+  id: prop<string | undefined>(() => undefined),
 }) {}
