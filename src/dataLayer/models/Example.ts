69280d566295f7ffe46f5c95ae95cd15ec962e02
Dominic Claßen <dominic@dominic-classen.de>
2022-07-02 10:16:43 +0200

    chore: generate pseudo random id for example model

--- src/dataLayer/models/Example.ts
+++ src/dataLayer/models/Example.ts
@@ -2,6 +2,6 @@ import { model, Model, prop } from "mobx-keystone";
 
 @model("Example")
 export default class Example extends Model({
-  id: prop<string | undefined>(() => "1"),
+  id: prop<string>(() => (Math.random() + 1).toString(36).substring(7)),
   header: prop<string | undefined>(() => "Init"),
 }) {}
